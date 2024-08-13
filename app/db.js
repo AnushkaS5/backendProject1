9ce7c461f518b34f2de02e01db7a3967f56c7583
Anushka Singh <anushkasj39@gmail.com>
2024-08-13 18:34:21 +0530

    first commit

---
+++ app/db.js
@@ -0,0 +1,12 @@
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const mongoose = require('mongoose');
+
+exports.connectMongoose =()=>{
+    mongoose.set("strictQuery", false);
+    mongoose.connect('mongodb://localhost:27017/habbit_tracker', { useNewUrlParser: true })
+    .then((e)=>console.log("Connected to Mongodb => Habit-Tracker"))
+    .catch((e)=>console.log("Not Connect Mongodb", e));
+}
